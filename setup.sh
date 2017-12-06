5849f839c5976bb89e0a9d545446abc909259d7e
Ryan Villarreal <XjCrazy09@users.noreply.github.com>
2017-12-06 12:44:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ chmod +x screen.sh
 
 # main file for running all other setup scripts and grabbing files
 apt-get update && apt-get upgrade && apt-get dist-upgrade
-apt-get install gobuster git
+apt-get install gobuster git 
+
+# move to the download folder for the following tools
+cd ~/Downloads/
+git clone https://github.com/danielmiessler/SecLists
