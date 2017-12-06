2d77f72ad9947d6308001bf6e19d263fe7386311
Ryan Villarreal <XjCrazy09@users.noreply.github.com>
2017-12-06 12:45:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# set the screensize
-chmod +x screen.sh
-./screen.sh
-
 # main file for running all other setup scripts and grabbing files
 apt-get update && apt-get upgrade && apt-get dist-upgrade
 apt-get install gobuster git 
+git clone https://github.com/XjCrazy09/KaliSetup
+cd KaliSetup
+
+# set the screensize
+chmod +x screen.sh
+./screen.sh
 
 # move to the download folder for the following tools
 cd ~/Downloads/
