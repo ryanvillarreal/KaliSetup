b9396278d219f581ac56ec8cf93928e2d45a7f4a
Ryan Villarreal <XjCrazy09@users.noreply.github.com>
2017-12-06 13:25:46 -0500

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# purge the old firefox
+# Purge the old firefox
 apt-get purge firefox
 apt-get autoclean
 unlink /usr/bin/firefox
 mv /usr/bin/firefox /usr/bin/firefox_bak
 
-# get the new firefox and link everything up
+# Get the new firefox and link everything up
 cd /usr/local
 pwd
 wget http://ftp.mozilla.org/pub/firefox/releases/57.0/linux-x86_64/en-US/firefox-57.0.tar.bz2
@@ -15,7 +15,7 @@ rm firefox-57.0.tar.bz2
 ln -s /usr/local/firefox/firefox /usr/bin/firefox
 rm /usr/share/applications/firefox-esr.desktop
 
-# now add the item to the favorites bar and the menu
+# Now add the item to the favorites bar and the menu
 echo "[Desktop Entry]
 Version=57.0
 Name=Firefox
