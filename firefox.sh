e52645aca437104427b3a307de85d608fce737b2
Ryan Villarreal <XjCrazy09@users.noreply.github.com>
2018-04-09 13:10:02 -0400

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -9,7 +9,7 @@ mv /usr/bin/firefox /usr/bin/firefox_bak
 # Get the new firefox and link everything up
 cd /usr/local
 pwd
-wget http://ftp.mozilla.org/pub/firefox/releases/57.0/linux-x86_64/en-US/firefox-59.0.tar.bz2
+wget http://ftp.mozilla.org/pub/firefox/releases/59.0/linux-x86_64/en-US/firefox-59.0.tar.bz2
 tar xvjf firefox-59.0.tar.bz2
 rm firefox-59.0.tar.bz2
 ln -s /usr/local/firefox/firefox /usr/bin/firefox
