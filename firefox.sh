b14578936db811fac59c07d2c8c6a106ffcb00d5
Ryan Villarreal <XjCrazy09@users.noreply.github.com>
2018-04-09 13:07:43 -0400

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -9,15 +9,15 @@ mv /usr/bin/firefox /usr/bin/firefox_bak
 # Get the new firefox and link everything up
 cd /usr/local
 pwd
-wget http://ftp.mozilla.org/pub/firefox/releases/57.0/linux-x86_64/en-US/firefox-57.0.tar.bz2
-tar xvjf firefox-57.0.tar.bz2
-rm firefox-57.0.tar.bz2
+wget http://ftp.mozilla.org/pub/firefox/releases/57.0/linux-x86_64/en-US/firefox-59.0.tar.bz2
+tar xvjf firefox-59.0.tar.bz2
+rm firefox-59.0.tar.bz2
 ln -s /usr/local/firefox/firefox /usr/bin/firefox
 rm /usr/share/applications/firefox-esr.desktop
 
 # Now add the item to the favorites bar and the menu
 echo "[Desktop Entry]
-Version=57.0
+Version=59.0
 Name=Firefox
 Comment=This is a test
 Exec=/usr/bin/firefox
