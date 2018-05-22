bdc6d14cfbf2e990996d57becc531e0266138942
Ryan Villarreal <999886+ryanvillarreal@users.noreply.github.com>
2018-05-22 17:53:36 -0400

    Update TrainingSetup.sh
    added dvwa and trying to fix networking restart

--- TrainingSetup.sh
+++ TrainingSetup.sh
@@ -13,9 +13,6 @@ apt-get install docker-ce -y
 systemctl start docker
 systemctl enable docker
 
-echo "Pause for Checking"
-read
-
 # Install Dnsmasq
 apt-get install dnsmasq -y 
 echo "# New Changes
@@ -23,17 +20,21 @@ listen-address=127.0.0.1
 bind-interfaces
 address=/.local/127.0.0.1
 " >> /etc/dnsmasq.conf
+systemctl enable dnsmasq
+systemctl restart dnsmasq
 
-echo "Pause for checking"
-read
+# Restart Networking
+service network-manager restart
 
 # Docker Pull down
 docker pull jwilder/nginx-proxy:latest
 docker pull bkimminich/juice-shop
 docker pull citizenstig/nowasp
+docker pull citizenstig/dvwa
 
 # Docker Config
 docker network create vuln
 docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro --name rev_proxy --net vuln --restart always jwilder/nginx-proxy
 docker run -d -e VIRTUAL_HOST=juice.local --net vuln --restart always bkimminich/juice-shop
 docker run -d -e VIRTUAL_HOST=mutillidae.local --net vuln --restart always citizenstig/nowasp
+docker run -d -e VIRTUAL_HOST=dvwa.local --net vuln --restart always citizenstig/dvwa
