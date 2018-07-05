b0ecc6201c440b4706bfc7cf1b2018922554c264
Ryan Villarreal <999886+ryanvillarreal@users.noreply.github.com>
2018-07-05 13:42:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,10 @@
 
 # main file for running all other setup scripts and grabbing files
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
-apt-get install gobuster git nodejs npm -y
+apt-get install gobuster git nodejs npm terminator -y
 cd ~/Downloads/
-git clone https://github.com/XjCrazy09/KaliSetup
+git clone https://github.com/ryanvillarreal/KaliSetup
 cd KaliSetup
-pwd
 
 # update any already exisiting tools
 wpscan --update
@@ -18,16 +17,16 @@ msfdb init
 cd ~/Downloads/KaliSetup/
 chmod +x ./firefox.sh
 ./firefox.sh
-pwd
 
-dconf write /org/gnome/shell/favorite-apps "['firefox.desktop','kali-burpsuite.desktop','org.gnome.Terminal.desktop']"
-
+dconf write /org/gnome/shell/favorite-apps "['firefox.desktop','kali-burpsuite.desktop','terminator.desktop']"
+cd ~/Downloads/KaliSetup/
+python linux-lumberjack.py
+su root
 
 # Move to the download folder for the following tools
 cd ~/Downloads/
 git clone https://github.com/danielmiessler/SecLists
 git clone https://github.com/ShawnDEvans/smbmap.git
-apt-get install terminator -y
 git clone https://github.com/jshaw87/Cheatsheets.git
 git clone https://github.com/SherifEldeeb/TinyMet.git
 git clone https://github.com/RUB-NDS/PRET.git
@@ -38,4 +37,4 @@ git clone https://github.com/cortesi/mitmproxy.git
 git clone https://github.com/BloodHoundAD/BloodHound.git
 git clone https://github.com/EmpireProject/Empire.git
 git clone https://github.com/guelfoweb/knock.git
-pwd
+
